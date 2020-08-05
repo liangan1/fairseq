17715cd61526c64772c415e2085dc99782c2015c
liangan1 <liangang.zhang@intel.com>
2020-08-05 15:30:42 +0800

    Modify run_training_cpu_accuracy.sh scripts for accuracy instead of performance

--- run_training_cpu_accuracy.sh
+++ run_training_cpu_accuracy.sh
@@ -62,7 +62,6 @@ fairseq-train $DATASET \
    --arch transformer_wmt_en_de_big --share-decoder-input-output-embed --optimizer adam \
    --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr 5e-4     --lr-scheduler inverse_sqrt \
    --warmup-updates 4000 --dropout 0.3 --weight-decay 0.0001 --criterion label_smoothed_cross_entropy \
-   --label-smoothing 0.1 --max-tokens $MAX_TOKEN \
-   $ARGS --validate-training-performance  --performance-begin-its 30 --performance-its-count 50 \
+   --label-smoothing 0.1 --max-tokens $MAX_TOKEN $ARGS \
    2>&1 |tee training_cpu_max_token_${MAX_TOKEN}_accuracy.log 
 
